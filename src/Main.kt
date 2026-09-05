3ef5c62a6700f2b637383317f29c23e85808db06
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-05 20:48:05 +0530

    Working on level 3

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,13 @@
 //package chucknorris
 fun main(){
-
+    println("Input string:")
+    val input = readln()
+    val finalbinary = StringBuilder()
+    for (i in input) {
+        val charToBinary = i.code.toString(2).toInt()
+        val binary = "%07d".format(charToBinary)
+        finalbinary.append(binary)
+    }
+    println("\nThe result:")
+    println(finalbinary)
 }
