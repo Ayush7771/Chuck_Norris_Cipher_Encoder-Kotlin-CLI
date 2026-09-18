e305543cd10bb97d65967d65eb73a6a4d5569e0c
Ayush Gupta <ayushgupta1371@gmail.com>
2026-09-18 08:55:48 +0530

    Level 4 completed

--- src/Main.kt
+++ src/Main.kt
@@ -1,37 +1,34 @@
 package chucknorris
 
 fun main() {
+    println("Input encoded string:")
     val input = readln()
+    println("\nThe result:")
     val chuckNorris = ChuckNorris(input)
-    chuckNorris.converttoBinary()
-    println(chuckNorris.binary)
+    println(chuckNorris.finalResult)
 }
 
 class ChuckNorris(input: String) {
 
-    val inputList: List<String> = input.split(" ")
-
+    val inputList: List<String> = input.trim().split(" ")
     val binary = StringBuilder()
+    val finalResult = StringBuilder()
 
-    fun converttoBinary() {
+    init {
         for (i in inputList.indices step 2) {
-            if (inputList[i] == "00") {
-                repeat(inputList[i + 1].length) {
-                    binary.append("0")
-                }
+            repeat(inputList[i + 1].length) {
+                binary.append(
+                    if (inputList[i] == "0") "1" else "0"
+                )
             }
-
-            if (inputList[i] == "0") {
-                repeat(inputList[i + 1].length) {
-                    binary.append("1")
-                }
-            }
-
-
         }
     }
 
-    val finalResult = StringBuilder()
 
-
+    init {
+        var sevenDigit = binary.chunked(7)
+        for (i in sevenDigit) {
+            finalResult.append(i.toInt(2).toChar())
+        }
+    }
 }
